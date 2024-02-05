1a4ee7c8e2de934b458d42a08dba3b29957002d0
Clay Zhu <clayzhu@163.com>
2024-02-05 17:05:55 +0800

    // TODO: zg LeetCode24Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day3/LeetCode24/LeetCode24VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day3/LeetCode24/LeetCode24VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode24VC.h"
 
+#include "LeetCode24Solution.h"
+
 @interface LeetCode24VC ()
 
 @end
@@ -16,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 24、两两交换链表中的节点
+    self.descLabel.text = @"给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。\n"
+    "示例 1：\n"
+    "   输入：head = [1,2,3,4]\n"
+    "   输出：[2,1,4,3]\n"
+    "示例 2：\n"
+    "   输入：head = []\n"
+    "   输出：[]\n"
+    "示例 3：\n"
+    "   输入：head = [1]\n"
+    "   输出：[1]\n"
+    "提示：\n"
+    "· 链表中节点的数目在范围 [0, 100] 内\n"
+    "· 0 <= Node.val <= 100\n";
 }
 
 /*
@@ -27,5 +43,9 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    
+}
 
 @end
