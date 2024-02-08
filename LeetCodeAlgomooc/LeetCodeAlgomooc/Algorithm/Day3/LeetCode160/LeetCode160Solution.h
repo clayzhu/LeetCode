af59311be9aa8ef3412509af88467f2dc4afa320
Clay Zhu <clayzhu@163.com>
2024-02-08 17:17:52 +0800

    LeetCode160Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day3/LeetCode160/LeetCode160Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day3/LeetCode160/LeetCode160Solution.h
@@ -16,4 +16,39 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+/// LeetCode 160、相交链表
+///
+/// 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+/// 图示两个链表在节点 c1 开始相交：
+/// 题目数据 保证 整个链式结构中不存在环。
+/// 注意，函数返回结果后，链表必须 保持其原始结构 。
+class LeetCode160Solution {
+    
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (headA == nullptr || headB == nullptr) {
+            return nullptr;
+        }
+        
+        ListNode* pA = headA;
+        ListNode* pB = headB;
+//        while (pA != pB) {  // 正确应该这一行：判断指针。为了方便 demo，临时改为判断内容
+        while ((pA != pB) && (pA->val != pB->val)) {
+            if (pA == nullptr) {
+                pA = headB;
+            } else {
+                pA = pA->next;
+            }
+            
+            if (pB == nullptr) {
+                pB = headA;
+            } else {
+                pB = pB->next;
+            }
+        }
+        return pA;
+    }
+    
+};
+
 #endif /* LeetCode160Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day3/LeetCode160/LeetCode160VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day3/LeetCode160/LeetCode160VC.mm
@@ -19,7 +19,12 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     // LeetCode 160、相交链表
-    self.descLabel.text = @"";
+    self.descLabel.text = @"给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。\n"
+    "图示两个链表在节点 c1 开始相交：\n"
+    "题目数据 保证 整个链式结构中不存在环。\n"
+    "注意，函数返回结果后，链表必须 保持其原始结构 。";
+    
+    self.outputTextView.text = @"请输入两行内容，分别是2个链表";
 }
 
 /*
@@ -31,5 +36,52 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    NSArray* inputLines = [self.inputTextView.text componentsSeparatedByString:@"\n"];
+    if (inputLines.count != 2) {
+        self.outputTextView.text = @"请输入两行内容，分别是2个链表";
+        return;
+    }
+    
+    // line 1
+    ListNode* firstNodeA = nullptr;
+    ListNode* formerNodeA = nullptr;
+    NSArray* line1Strs = [inputLines[0] componentsSeparatedByString:@","];
+    for (int i = 0; i < line1Strs.count; i ++) {
+        int num = [line1Strs[i] intValue];
+        ListNode* currentNode = new ListNode(num);
+        if (i == 0) {
+            firstNodeA = currentNode;
+        } else {
+            formerNodeA->next = currentNode;
+        }
+        formerNodeA = currentNode;
+    }
+    
+    // line 2
+    ListNode* firstNodeB = nullptr;
+    ListNode* formerNodeB = nullptr;
+    NSArray* line2Strs = [inputLines[1] componentsSeparatedByString:@","];
+    for (int i = 0; i < line2Strs.count; i ++) {
+        int num = [line2Strs[i] intValue];
+        ListNode* currentNode = new ListNode(num);
+        if (i == 0) {
+            firstNodeB = currentNode;
+        } else {
+            formerNodeB->next = currentNode;
+        }
+        formerNodeB = currentNode;
+    }
+    
+    LeetCode160Solution solution;
+    ListNode* resultNode = solution.getIntersectionNode(firstNodeA, firstNodeB);
+    
+    if (resultNode == nullptr) {
+        self.outputTextView.text = @"nullptr";
+    } else {
+        self.outputTextView.text = [NSString stringWithFormat:@"%@", @(resultNode->val)];
+    }
+}
 
 @end
