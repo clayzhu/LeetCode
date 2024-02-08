2b29b14f12e3a4a11b4220fdd57a403ab9d1d241
Clay Zhu <clayzhu@163.com>
2024-02-08 23:16:45 +0800

    impl LeetCode21Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day4/LeetCode21/LeetCode21Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day4/LeetCode21/LeetCode21Solution.h
@@ -8,5 +8,58 @@
 #ifndef LeetCode21Solution_h
 #define LeetCode21Solution_h
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/// LeetCode 21、合并两个有序链表
+///
+/// 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+/// 示例 1：
+///    输入：l1 = [1,2,4], l2 = [1,3,4]
+///    输出：[1,1,2,3,4,4]
+/// 示例 2：
+///    输入：l1 = [], l2 = []
+///    输出：[]
+/// 示例 3：
+///    输入：l1 = [], l2 = [0]
+///    输出：[0]
+/// 提示：
+/// · 两个链表的节点数目范围是 [0, 50]
+/// · -100 <= Node.val <= 100
+/// · l1 和 l2 均按 非递减顺序 排列
+class LeetCode21Solution {
+    
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* pre = dummy;
+        ListNode* p1 = list1;
+        ListNode* p2 = list2;
+        while (p1 != nullptr && p2 != nullptr) {
+            if (p1->val <= p2->val) {
+                pre->next = p1;
+                p1 = p1->next;
+            } else {
+                pre->next = p2;
+                p2 = p2->next;
+            }
+            pre = pre->next;
+        }
+        
+        if (p1 != nullptr) {
+            pre->next = p1;
+        }
+        if (p2 != nullptr) {
+            pre->next = p2;
+        }
+        return dummy->next;
+    }
+    
+};
 
 #endif /* LeetCode21Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day4/LeetCode21/LeetCode21VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day4/LeetCode21/LeetCode21VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode21VC.h"
 
+#include "LeetCode21Solution.h"
+
 @interface LeetCode21VC ()
 
 @end
@@ -16,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 21、合并两个有序链表
+    self.descLabel.text = @"将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。\n"
+    "示例 1：\n"
+    "   输入：l1 = [1,2,4], l2 = [1,3,4]\n"
+    "   输出：[1,1,2,3,4,4]\n"
+    "示例 2：\n"
+    "   输入：l1 = [], l2 = []\n"
+    "   输出：[]\n"
+    "示例 3：\n"
+    "   输入：l1 = [], l2 = [0]\n"
+    "   输出：[0]\n"
+    "提示：\n"
+    "· 两个链表的节点数目范围是 [0, 50]\n"
+    "· -100 <= Node.val <= 100\n"
+    "· l1 和 l2 均按 非递减顺序 排列";
 }
 
 /*
@@ -27,5 +44,55 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    NSArray* inputLines = [self.inputTextView.text componentsSeparatedByString:@"\n"];
+    if (inputLines.count != 2) {
+        self.outputTextView.text = @"请输入两行内容，每行分别是一个链表";
+        return;
+    }
+    
+    // line 1
+    ListNode* firstNodeA = nullptr;
+    ListNode* formerNodeA = nullptr;
+    NSArray* line1Strs = [inputLines[0] componentsSeparatedByString:@","];
+    for (int i = 0; i < line1Strs.count; i ++) {
+        int num = [line1Strs[i] intValue];
+        ListNode* currentNode = new ListNode(num);
+        if (i == 0) {
+            firstNodeA = currentNode;
+        } else {
+            formerNodeA->next = currentNode;
+        }
+        formerNodeA = currentNode;
+    }
+    
+    // line 2
+    ListNode* firstNodeB = nullptr;
+    ListNode* formerNodeB = nullptr;
+    NSArray* line2Strs = [inputLines[1] componentsSeparatedByString:@","];
+    for (int i = 0; i < line2Strs.count; i ++) {
+        int num = [line2Strs[i] intValue];
+        ListNode* currentNode = new ListNode(num);
+        if (i == 0) {
+            firstNodeB = currentNode;
+        } else {
+            formerNodeB->next = currentNode;
+        }
+        formerNodeB = currentNode;
+    }
+    
+    LeetCode21Solution solution;
+    ListNode* resultFirstNode = solution.mergeTwoLists(firstNodeA, firstNodeB);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    ListNode* resultCurrentNode = resultFirstNode;
+    while (resultCurrentNode != nullptr) {
+        [outputNums addObject:@(resultCurrentNode->val)];
+        resultCurrentNode = resultCurrentNode->next;
+    }
+    NSString* output = [outputNums componentsJoinedByString:@","];
+    self.outputTextView.text = output;
+}
 
 @end
