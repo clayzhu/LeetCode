b097c2531de1fd42bdb4e65be90337900ecad950
Clay Zhu <clayzhu@163.com>
2024-02-22 00:21:35 +0800

    LeetCode206Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day29/LeetCode206/LeetCode206Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day29/LeetCode206/LeetCode206Solution.h
@@ -8,5 +8,61 @@
 #ifndef LeetCode206Solution_h
 #define LeetCode206Solution_h
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/// LeetCode 206、反转链表
+///
+/// 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+/// 示例 1：
+///    输入：head = [1,2,3,4,5]
+///    输出：[5,4,3,2,1]
+/// 示例 2：
+///    输入：head = [1,2]
+///    输出：[2,1]
+/// 示例 3：
+///    输入：head = []
+///    输出：[]
+/// 提示：
+/// · 链表中节点的数目范围是 [0, 5000]
+/// · -5000 <= Node.val <= 5000
+/// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+class LeetCode206Solution {
+    
+public:
+    ListNode* reverseList(ListNode* head) {
+#if 1   // 迭代
+        if (head == nullptr) {
+            return nullptr;
+        }
+        
+        ListNode* cur = head, *pre = nullptr, *temp = nullptr;
+        while (cur != nullptr) {
+            temp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        
+        return pre;
+#else   // 递归
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        
+        ListNode* cur = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        
+        return cur;
+#endif
+    }
+    
+};
 
 #endif /* LeetCode206Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day29/LeetCode206/LeetCode206VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day29/LeetCode206/LeetCode206VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode206VC.h"
 
+#include "LeetCode206Solution.h"
+
 @interface LeetCode206VC ()
 
 @end
@@ -16,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 206、反转链表
+    self.descLabel.text = @"给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。\n"
+    "示例 1：\n"
+    "   输入：head = [1,2,3,4,5]\n"
+    "   输出：[5,4,3,2,1]\n"
+    "示例 2：\n"
+    "   输入：head = [1,2]\n"
+    "   输出：[2,1]\n"
+    "示例 3：\n"
+    "   输入：head = []\n"
+    "   输出：[]\n"
+    "提示：\n"
+    "· 链表中节点的数目范围是 [0, 5000]\n"
+    "· -5000 <= Node.val <= 5000\n"
+    "进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？";
 }
 
 /*
@@ -27,5 +44,33 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    ListNode* firstNode = nullptr;
+    ListNode* formerNode = nullptr;
+    NSArray* inputStrs = [self.inputTextView.text componentsSeparatedByString:@","];
+    for (int i = 0; i < inputStrs.count; i ++) {
+        int num = [inputStrs[i] intValue];
+        ListNode* currentNode = new ListNode(num);
+        if (i == 0) {
+            firstNode = currentNode;
+        } else {
+            formerNode->next = currentNode;
+        }
+        formerNode = currentNode;
+    }
+    
+    LeetCode206Solution solution;
+    ListNode* resultFirstNode = solution.reverseList(firstNode);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    ListNode* resultCurrentNode = resultFirstNode;
+    while (resultCurrentNode != nullptr) {
+        [outputNums addObject:@(resultCurrentNode->val)];
+        resultCurrentNode = resultCurrentNode->next;
+    }
+    NSString* output = [outputNums componentsJoinedByString:@","];
+    self.outputTextView.text = output;
+}
 
 @end
