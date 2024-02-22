dcbb19912c9481f85038478970ccaf78881bda01
Clay Zhu <clayzhu@163.com>
2024-02-23 00:58:54 +0800

    LeetCode876Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day4/LeetCode876/LeetCode876Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day4/LeetCode876/LeetCode876Solution.h
@@ -8,5 +8,41 @@
 #ifndef LeetCode876Solution_h
 #define LeetCode876Solution_h
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/// LeetCode 876、链表的中间结点
+///
+/// 给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+/// 如果有两个中间结点，则返回第二个中间结点。
+/// 示例 1：
+///    输入：head = [1,2,3,4,5]
+///    输出：[3,4,5]
+///    解释：链表只有一个中间结点，值为 3 。
+/// 示例 2：
+///    输入：head = [1,2,3,4,5,6]
+///    输出：[4,5,6]
+///    解释：该链表有两个中间结点，值分别为 3 和 4 ，返回第二个结点。
+/// 提示：
+/// · 链表的结点数范围是 [1, 100]
+/// · 1 <= Node.val <= 100
+class LeetCode876Solution {
+    
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head, *fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    
+};
 
 #endif /* LeetCode876Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day4/LeetCode876/LeetCode876VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day4/LeetCode876/LeetCode876VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode876VC.h"
 
+#include "LeetCode876Solution.h"
+
 @interface LeetCode876VC ()
 
 @end
@@ -16,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 876、链表的中间结点
+    self.descLabel.text = @"给你单链表的头结点 head ，请你找出并返回链表的中间结点。\n"
+    "如果有两个中间结点，则返回第二个中间结点。\n"
+    "示例 1：\n"
+    "   输入：head = [1,2,3,4,5]\n"
+    "   输出：[3,4,5]\n"
+    "   解释：链表只有一个中间结点，值为 3 。\n"
+    "示例 2：\n"
+    "   输入：head = [1,2,3,4,5,6]\n"
+    "   输出：[4,5,6]\n"
+    "   解释：该链表有两个中间结点，值分别为 3 和 4 ，返回第二个结点。\n"
+    "提示：\n"
+    "· 链表的结点数范围是 [1, 100]\n"
+    "· 1 <= Node.val <= 100\n";
 }
 
 /*
@@ -27,5 +43,33 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    ListNode* firstNode = nullptr;
+    ListNode* formerNode = nullptr;
+    NSArray* inputStrs = [self.inputTextView.text componentsSeparatedByString:@","];
+    for (int i = 0; i < inputStrs.count; i ++) {
+        int num = [inputStrs[i] intValue];
+        ListNode* currentNode = new ListNode(num);
+        if (i == 0) {
+            firstNode = currentNode;
+        } else {
+            formerNode->next = currentNode;
+        }
+        formerNode = currentNode;
+    }
+    
+    LeetCode876Solution solution;
+    ListNode* resultFirstNode = solution.middleNode(firstNode);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    ListNode* resultCurrentNode = resultFirstNode;
+    while (resultCurrentNode != nullptr) {
+        [outputNums addObject:@(resultCurrentNode->val)];
+        resultCurrentNode = resultCurrentNode->next;
+    }
+    NSString* output = [outputNums componentsJoinedByString:@","];
+    self.outputTextView.text = output;
+}
 
 @end
