fb7cbb107815548021f2fb59654c77b0dd3fa7b6
Clay Zhu <clayzhu@163.com>
2024-02-27 22:06:34 +0800

    LeetCode144Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day36/LeetCode144/LeetCode144Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day36/LeetCode144/LeetCode144Solution.h
@@ -8,5 +8,110 @@
 #ifndef LeetCode144Solution_h
 #define LeetCode144Solution_h
 
+#include <stack>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/// LeetCode 144、二叉树的前序遍历
+///
+/// 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+/// 示例 1：
+///    输入：root = [1,null,2,3]
+///    输出：[1,2,3]
+/// 示例 2：
+///    输入：root = []
+///    输出：[]
+/// 示例 3：
+///    输入：root = [1]
+///    输出：[1]
+/// 示例 4：
+///    输入：root = [1,2]
+///    输出：[1,2]
+/// 示例 5：
+///    输入：root = [1,null,2]
+///    输出：[1,2]
+/// 提示：
+/// · 树中节点数目在范围 [0, 100] 内
+/// · -100 <= Node.val <= 100
+/// 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+class LeetCode144Solution {
+    
+public:
+    std::vector<int> preorderTraversal(TreeNode* root) {
+#if 0   // 递归算法
+        std::vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        preorder(root, &result);
+        
+        return result;
+#elif 0 // 迭代算法
+        std::vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        TreeNode* node = root;
+        std::stack<TreeNode*> stack;
+        while (node || !stack.empty()) {
+            if (node != nullptr) {
+                result.emplace_back(node->val);
+                stack.push(node);
+                node = node->left;
+            } else {
+                node = stack.top();
+                stack.pop();
+                node = node->right;
+            }
+        }
+        
+        return result;
+#elif 1 // 迭代算法
+        std::vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        TreeNode* node = root;
+        std::stack<TreeNode*> stack;
+        while (node != nullptr || !stack.empty()) {
+            while (node != nullptr) {
+                result.emplace_back(node->val);
+                stack.push(node);
+                node = node->left;
+            }
+            
+            node = stack.top();
+            stack.pop();
+            node = node->right;
+        }
+        
+        return result;
+#endif
+    }
+    
+private:
+    // 递归算法
+    void preorder(TreeNode* node, std::vector<int>* out_result) {
+        if (node == nullptr) {
+            return;
+        }
+        
+        out_result->emplace_back(node->val);
+        preorder(node->left, out_result);
+        preorder(node->right, out_result);
+    }
+    
+};
 
 #endif /* LeetCode144Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day36/LeetCode144/LeetCode144VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day36/LeetCode144/LeetCode144VC.mm
@@ -7,6 +7,10 @@
 
 #import "LeetCode144VC.h"
 
+#include <queue>
+
+#include "LeetCode144Solution.h"
+
 @interface LeetCode144VC ()
 
 @end
@@ -16,6 +20,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 144、二叉树的前序遍历
+    self.descLabel.text = @"给你二叉树的根节点 root ，返回它节点值的 前序 遍历。\n"
+    "示例 1：\n"
+    "   输入：root = [1,null,2,3]\n"
+    "   输出：[1,2,3]\n"
+    "示例 2：\n"
+    "   输入：root = []\n"
+    "   输出：[]\n"
+    "示例 3：\n"
+    "   输入：root = [1]\n"
+    "   输出：[1]\n"
+    "示例 4：\n"
+    "   输入：root = [1,2]\n"
+    "   输出：[1,2]\n"
+    "示例 5：\n"
+    "   输入：root = [1,null,2]\n"
+    "   输出：[1,2]\n"
+    "提示：\n"
+    "· 树中节点数目在范围 [0, 100] 内\n"
+    "· -100 <= Node.val <= 100\n"
+    "进阶：递归算法很简单，你可以通过迭代算法完成吗？\n";
 }
 
 /*
@@ -27,5 +52,59 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    TreeNode* root = nullptr;
+    std::queue<TreeNode*> queue;
+    int index = 0;
+    
+    NSArray* inputStrs = [self.inputTextView.text componentsSeparatedByString:@","];
+    if (inputStrs.count > 0 && ![inputStrs[index] isEqualToString:@"null"]) {
+        root = new TreeNode([inputStrs[index] intValue]);
+        queue.push(root);
+        
+        index ++;
+    } else {
+        self.outputTextView.text = @"根结点输入有误";
+        return;
+    }
+    
+    while (!queue.empty()) {
+        TreeNode* firstNode = queue.front();
+        queue.pop();
+        
+        if (index < inputStrs.count) {
+            if ([inputStrs[index] isEqualToString:@"null"]) {
+                firstNode->left = nullptr;
+            } else {
+                TreeNode* node = new TreeNode([inputStrs[index] intValue]);
+                firstNode->left = node;
+                queue.push(firstNode->left);
+            }
+            index ++;
+        }
+        
+        if (index < inputStrs.count) {
+            if ([inputStrs[index] isEqualToString:@"null"]) {
+                firstNode->right = nullptr;
+            } else {
+                TreeNode* node = new TreeNode([inputStrs[index] intValue]);
+                firstNode->right = node;
+                queue.push(firstNode->right);
+            }
+            index ++;
+        }
+    }
+    
+    LeetCode144Solution solution;
+    std::vector<int> result = solution.preorderTraversal(root);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    for (int i = 0; i < result.size(); i ++) {
+        [outputNums addObject:@(result[i])];
+    }
+    NSString* output = [outputNums componentsJoinedByString:@","];
+    self.outputTextView.text = output;
+}
 
 @end
