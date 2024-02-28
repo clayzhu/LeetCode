5e4d10120b2667bc5d421ee5269a309d2a6e3792
Clay Zhu <clayzhu@163.com>
2024-02-28 20:10:39 +0800

    LeetCode102Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day36/LeetCode102/LeetCode102Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day36/LeetCode102/LeetCode102Solution.h
@@ -8,5 +8,69 @@
 #ifndef LeetCode102Solution_h
 #define LeetCode102Solution_h
 
+#include <queue>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/// LeetCode 102、二叉树的层序遍历
+///
+/// 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+/// 示例 1：
+///    输入：root = [3,9,20,null,null,15,7]
+///    输出：[[3],[9,20],[15,7]]
+/// 示例 2：
+///    输入：root = [1]
+///    输出：[[1]]
+/// 示例 3：
+///    输入：root = []
+///    输出：[]
+/// 提示：
+/// · 树中节点数目在范围 [0, 2000] 内
+/// · -1000 <= Node.val <= 1000
+class LeetCode102Solution {
+    
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode* root) {
+        std::vector<std::vector<int>> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        std::queue<TreeNode*> queue;
+        queue.push(root);
+        
+        while (!queue.empty()) {
+            int size = queue.size();
+            std::vector<int> level;
+            
+            for (int i = 0; i < size; i ++) {
+                TreeNode* first_node = queue.front();
+                queue.pop();
+                level.emplace_back(first_node->val);
+                
+                if (first_node->left != nullptr) {
+                    queue.push(first_node->left);
+                }
+                
+                if (first_node->right != nullptr) {
+                    queue.push(first_node->right);
+                }
+            }
+            
+            result.emplace_back(level);
+        }
+        
+        return result;
+    }
+    
+};
 
 #endif /* LeetCode102Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day36/LeetCode102/LeetCode102VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day36/LeetCode102/LeetCode102VC.mm
@@ -7,6 +7,10 @@
 
 #import "LeetCode102VC.h"
 
+#include <queue>
+
+#include "LeetCode102Solution.h"
+
 @interface LeetCode102VC ()
 
 @end
@@ -16,6 +20,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 102、二叉树的层序遍历
+    self.descLabel.text = @"给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。\n"
+    "示例 1：\n"
+    "   输入：root = [3,9,20,null,null,15,7]\n"
+    "   输出：[[3],[9,20],[15,7]]\n"
+    "示例 2：\n"
+    "   输入：root = [1]\n"
+    "   输出：[[1]]\n"
+    "示例 3：\n"
+    "   输入：root = []\n"
+    "   输出：[]\n"
+    "提示：\n"
+    "· 树中节点数目在范围 [0, 2000] 内\n"
+    "· -1000 <= Node.val <= 1000\n";
 }
 
 /*
@@ -27,5 +45,66 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    TreeNode* root = nullptr;
+    std::queue<TreeNode*> queue;
+    int index = 0;
+    
+    NSArray* inputStrs = [self.inputTextView.text componentsSeparatedByString:@","];
+    if (inputStrs.count > 0 && ![inputStrs[index] isEqualToString:@"null"]) {
+        root = new TreeNode([inputStrs[index] intValue]);
+        queue.push(root);
+        
+        index ++;
+    } else {
+        self.outputTextView.text = @"根结点输入有误";
+        return;
+    }
+    
+    while (!queue.empty()) {
+        TreeNode* firstNode = queue.front();
+        queue.pop();
+        
+        if (index < inputStrs.count) {
+            if ([inputStrs[index] isEqualToString:@"null"]) {
+                firstNode->left = nullptr;
+            } else {
+                TreeNode* node = new TreeNode([inputStrs[index] intValue]);
+                firstNode->left = node;
+                queue.push(firstNode->left);
+            }
+            index ++;
+        }
+        
+        if (index < inputStrs.count) {
+            if ([inputStrs[index] isEqualToString:@"null"]) {
+                firstNode->right = nullptr;
+            } else {
+                TreeNode* node = new TreeNode([inputStrs[index] intValue]);
+                firstNode->right = node;
+                queue.push(firstNode->right);
+            }
+            index ++;
+        }
+    }
+    
+    LeetCode102Solution solution;
+    std::vector<std::vector<int>> result = solution.levelOrder(root);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    for (int i = 0; i < result.size(); i ++) {
+        NSMutableArray* levelNums = [NSMutableArray array];
+        for (int j = 0; j < result[i].size(); j ++) {
+            [levelNums addObject:@(result[i][j])];
+        }
+        NSString* level = [NSString stringWithFormat:@"[%@]",
+                           [levelNums componentsJoinedByString:@","]];
+        [outputNums addObject:level];
+    }
+    NSString* output = [NSString stringWithFormat:@"[%@]",
+                        [outputNums componentsJoinedByString:@","]];
+    self.outputTextView.text = output;
+}
 
 @end
