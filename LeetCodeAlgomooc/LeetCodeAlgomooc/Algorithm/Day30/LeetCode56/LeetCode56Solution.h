ee070118109e4c472a5fdd3122af1faf13bac097
Clay Zhu <clayzhu@163.com>
2024-02-29 23:35:55 +0800

    LeetCode56Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day30/LeetCode56/LeetCode56Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day30/LeetCode56/LeetCode56Solution.h
@@ -8,5 +8,47 @@
 #ifndef LeetCode56Solution_h
 #define LeetCode56Solution_h
 
+#include <vector>
+
+/// LeetCode 56、合并区间
+///
+/// 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+/// 示例 1：
+///    输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+///    输出：[[1,6],[8,10],[15,18]]
+///    解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+/// 示例 2：
+///    输入：intervals = [[1,4],[4,5]]
+///    输出：[[1,5]]
+///    解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+/// 提示：
+/// · 1 <= intervals.length <= 104
+/// · intervals[i].length == 2
+/// · 0 <= starti <= endi <= 104
+class LeetCode56Solution {
+    
+public:
+    std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals) {
+        if (intervals.size() <= 0) {
+            return {};
+        }
+        
+        std::sort(intervals.begin(), intervals.end());
+        
+        std::vector<std::vector<int>> merged_intervals;
+        for (int i = 0; i < intervals.size(); i ++) {
+            int left = intervals[i][0], right = intervals[i][1];
+            if (merged_intervals.size() == 0 || merged_intervals.back()[1] < left) {
+//                merged_intervals.emplace_back({left, right});   // error: emplace_back 直接在参数中构造 vector 会报错
+                merged_intervals.push_back({left, right});
+            } else {
+                merged_intervals.back()[1] = std::max(merged_intervals.back()[1], right);
+            }
+        }
+        
+        return merged_intervals;
+    }
+    
+};
 
 #endif /* LeetCode56Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day30/LeetCode56/LeetCode56VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day30/LeetCode56/LeetCode56VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode56VC.h"
 
+#include "LeetCode56Solution.h"
+
 @interface LeetCode56VC ()
 
 @end
@@ -16,6 +18,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 56、合并区间
+    self.descLabel.text = @"以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。\n"
+    "示例 1：\n"
+    "   输入：intervals = [[1,3],[2,6],[8,10],[15,18]]\n"
+    "   输出：[[1,6],[8,10],[15,18]]\n"
+    "   解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].\n"
+    "示例 2：\n"
+    "   输入：intervals = [[1,4],[4,5]]\n"
+    "   输出：[[1,5]]\n"
+    "   解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。\n"
+    "提示：\n"
+    "· 1 <= intervals.length <= 104\n"
+    "· intervals[i].length == 2\n"
+    "· 0 <= starti <= endi <= 104";
+    
+    self.outputTextView.text = @"请输入多行内容，每行一个区间，“[]”符号不需要写";
 }
 
 /*
@@ -27,5 +45,32 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    NSArray* inputLines = [self.inputTextView.text componentsSeparatedByString:@"\n"];
+    std::vector<std::vector<int>> intervals;
+    for (NSString* line in inputLines) {
+        NSArray* nums = [line componentsSeparatedByString:@","];
+        if (nums.count != 2) {
+            continue;
+        }
+        std::vector<int> interval {[nums[0] intValue], [nums[1] intValue]};
+        intervals.emplace_back(std::move(interval));
+    }
+    
+    LeetCode56Solution solution;
+    std::vector<std::vector<int>> result = solution.merge(intervals);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    for (auto& interval : result) {
+        NSArray* intervalArray = @[@(interval[0]), @(interval[1])];
+        NSString* intervalStr = [NSString stringWithFormat:@"[%@]",
+                                 [intervalArray componentsJoinedByString:@","]];
+        [outputNums addObject:intervalStr];
+    }
+    NSString* output = [NSString stringWithFormat:@"[%@]",
+                        [outputNums componentsJoinedByString:@","]];
+    self.outputTextView.text = output;
+}
 
 @end
