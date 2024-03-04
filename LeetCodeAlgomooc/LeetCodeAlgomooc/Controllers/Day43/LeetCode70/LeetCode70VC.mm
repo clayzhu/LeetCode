285ec4f8af8bfe4304a3f4a6f85bed8c38740c34
Clay Zhu <clayzhu@163.com>
2024-03-04 22:55:43 +0800

    LeetCode70Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day43/LeetCode70/LeetCode70VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day43/LeetCode70/LeetCode70VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode70VC.h"
 
+#include "LeetCode70Solution.h"
+
 @interface LeetCode70VC ()
 
 @end
@@ -16,6 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 70、爬楼梯
+    self.descLabel.text = @"假设你正在爬楼梯。需要 n 阶你才能到达楼顶。\n"
+    "每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？\n"
+    "示例 1：\n"
+    "   输入：n = 2\n"
+    "   输出：2\n"
+    "   解释：有两种方法可以爬到楼顶。\n"
+    "   1. 1 阶 + 1 阶\n"
+    "   2. 2 阶\n"
+    "示例 2：\n"
+    "   输入：n = 3\n"
+    "   输出：3\n"
+    "   解释：有三种方法可以爬到楼顶。\n"
+    "   1. 1 阶 + 1 阶 + 1 阶\n"
+    "   2. 1 阶 + 2 阶\n"
+    "   3. 2 阶 + 1 阶\n"
+    "提示：\n"
+    "· 1 <= n <= 45";
 }
 
 /*
@@ -27,5 +47,18 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    int n = [self.inputTextView.text intValue];
+    if (n <= 0) {
+        self.outputTextView.text = @"请输入 1 <= n <= 45 范围内的数字";
+        return;
+    }
+    
+    LeetCode70Solution solution;
+    int result = solution.climbStairs(n);
+    
+    self.outputTextView.text = [@(result) stringValue];
+}
 
 @end
