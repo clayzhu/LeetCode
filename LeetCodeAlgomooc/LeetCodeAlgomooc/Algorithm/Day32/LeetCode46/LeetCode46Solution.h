0b68310d3036f7c4cf39ab1af2926c13d6b69ccf
Clay Zhu <clayzhu@163.com>
2024-03-14 23:23:39 +0800

    LeetCode46Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day32/LeetCode46/LeetCode46Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day32/LeetCode46/LeetCode46Solution.h
@@ -8,5 +8,54 @@
 #ifndef LeetCode46Solution_h
 #define LeetCode46Solution_h
 
+#include <vector>
+
+/// LeetCode 46、全排列
+///
+/// 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+/// 示例 1：
+///    输入：nums = [1,2,3]
+///    输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+/// 示例 2：
+///    输入：nums = [0,1]
+///    输出：[[0,1],[1,0]]
+/// 示例 3：
+///    输入：nums = [1]
+///    输出：[[1]]
+/// 提示：
+/// · 1 <= nums.length <= 6
+/// · -10 <= nums[i] <= 10
+/// · nums 中的所有整数 互不相同
+class LeetCode46Solution {
+    
+public:
+    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+        std::vector<int> path;
+        std::vector<bool> used(nums.size(), false);
+        backtrack(nums, path, used);
+        return result_;
+    }
+    
+private:
+    std::vector<std::vector<int>> result_;
+    
+    void backtrack(std::vector<int>& nums, std::vector<int>& path, std::vector<bool>& used) {
+        if (path.size() == nums.size()) {
+            result_.push_back(path);
+            return;
+        }
+        
+        for (int i = 0; i < nums.size(); i ++) {
+            if (used[i] == false) {
+                path.push_back(nums[i]);
+                used[i] = true;
+                backtrack(nums, path, used);
+                used[i] = false;
+                path.pop_back();
+            }
+        }
+    }
+    
+};
 
 #endif /* LeetCode46Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day32/LeetCode46/LeetCode46VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day32/LeetCode46/LeetCode46VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode46VC.h"
 
+#include "LeetCode46Solution.h"
+
 @interface LeetCode46VC ()
 
 @end
@@ -16,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 46、全排列
+    self.descLabel.text = @"给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。\n"
+    "示例 1：\n"
+    "   输入：nums = [1,2,3]\n"
+    "   输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]\n"
+    "示例 2：\n"
+    "   输入：nums = [0,1]\n"
+    "   输出：[[0,1],[1,0]]\n"
+    "示例 3：\n"
+    "   输入：nums = [1]\n"
+    "   输出：[[1]]\n"
+    "提示：\n"
+    "· 1 <= nums.length <= 6\n"
+    "· -10 <= nums[i] <= 10\n"
+    "· nums 中的所有整数 互不相同";
 }
 
 /*
@@ -27,5 +44,30 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    std::vector<int> nums;
+    NSArray* inputStrs = [self.inputTextView.text componentsSeparatedByString:@","];
+    for (NSString* numStr in inputStrs) {
+        nums.emplace_back([numStr intValue]);
+    }
+    
+    LeetCode46Solution solution;
+    std::vector<std::vector<int>> result = solution.permute(nums);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    for (auto& interval : result) {
+        NSMutableArray* intervalArray = [NSMutableArray array];
+        for (int num : interval) {
+            [intervalArray addObject:@(num)];
+        }
+        NSString* intervalStr = [NSString stringWithFormat:@"[%@]",
+                                 [intervalArray componentsJoinedByString:@","]];
+        [outputNums addObject:intervalStr];
+    }
+    NSString* output = [NSString stringWithFormat:@"[%@]",
+                        [outputNums componentsJoinedByString:@","]];
+    self.outputTextView.text = output;
+}
 
 @end
