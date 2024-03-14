7e6456475f9fa0ed0734ff48bef42f0460e73cf4
Clay Zhu <clayzhu@163.com>
2024-03-14 21:49:39 +0800

    LeetCode90Solution

--- LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day32/LeetCode90/LeetCode90Solution.h
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Algorithm/Day32/LeetCode90/LeetCode90Solution.h
@@ -8,5 +8,49 @@
 #ifndef LeetCode90Solution_h
 #define LeetCode90Solution_h
 
+#include <vector>
+
+/// LeetCode 90、子集 II
+///
+/// 给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。
+/// 解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。
+/// 示例 1：
+///    输入：nums = [1,2,2]
+///    输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]
+/// 示例 2：
+///    输入：nums = [0]
+///    输出：[[],[0]]
+/// 提示：
+/// · 1 <= nums.length <= 10
+/// · -10 <= nums[i] <= 10
+class LeetCode90Solution {
+    
+public:
+    std::vector<std::vector<int>> subsetsWithDup(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        
+        std::vector<int> path;
+        backtrack(nums, path, 0);
+        return result_;
+    }
+    
+private:
+    std::vector<std::vector<int>> result_;
+    
+    void backtrack(std::vector<int>& nums, std::vector<int>& path, int start) {
+        result_.push_back(path);
+        
+        for (int i = start; i < nums.size(); i ++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            path.push_back(nums[i]);
+            backtrack(nums, path, i + 1);
+            path.pop_back();
+        }
+    }
+    
+};
 
 #endif /* LeetCode90Solution_h */
--- LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day32/LeetCode90/LeetCode90VC.mm
+++ LeetCodeAlgomooc/LeetCodeAlgomooc/Controllers/Day32/LeetCode90/LeetCode90VC.mm
@@ -7,6 +7,8 @@
 
 #import "LeetCode90VC.h"
 
+#include "LeetCode90Solution.h"
+
 @interface LeetCode90VC ()
 
 @end
@@ -16,6 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // LeetCode 90、子集 II
+    self.descLabel.text = @"给你一个整数数组 nums ，其中可能包含重复元素，请你返回该数组所有可能的子集（幂集）。\n"
+    "解集 不能 包含重复的子集。返回的解集中，子集可以按 任意顺序 排列。\n"
+    "示例 1：\n"
+    "   输入：nums = [1,2,2]\n"
+    "   输出：[[],[1],[1,2],[1,2,2],[2],[2,2]]\n"
+    "示例 2：\n"
+    "   输入：nums = [0]\n"
+    "   输出：[[],[0]]\n"
+    "提示：\n"
+    "· 1 <= nums.length <= 10\n"
+    "· -10 <= nums[i] <= 10";
 }
 
 /*
@@ -27,5 +41,30 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (void)exeAlgorithm {
+    std::vector<int> nums;
+    NSArray* inputStrs = [self.inputTextView.text componentsSeparatedByString:@","];
+    for (NSString* numStr in inputStrs) {
+        nums.emplace_back([numStr intValue]);
+    }
+    
+    LeetCode90Solution solution;
+    std::vector<std::vector<int>> result = solution.subsetsWithDup(nums);
+    
+    NSMutableArray* outputNums = [NSMutableArray array];
+    for (auto& interval : result) {
+        NSMutableArray* intervalArray = [NSMutableArray array];
+        for (int num : interval) {
+            [intervalArray addObject:@(num)];
+        }
+        NSString* intervalStr = [NSString stringWithFormat:@"[%@]",
+                                 [intervalArray componentsJoinedByString:@","]];
+        [outputNums addObject:intervalStr];
+    }
+    NSString* output = [NSString stringWithFormat:@"[%@]",
+                        [outputNums componentsJoinedByString:@","]];
+    self.outputTextView.text = output;
+}
 
 @end
